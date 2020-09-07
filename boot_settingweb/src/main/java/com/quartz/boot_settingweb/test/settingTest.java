cc8b6c56aaba248fb98706bf5f6966fb8d2f6bfe
Seul <seulee95@naver.com>
2020-09-08 00:53:47 +0900

    200908 | 3주차 스프링부트 환경설정 완료

---
+++ boot_settingweb/src/main/java/com/quartz/boot_settingweb/test/settingTest.java
@@ -0,0 +1,30 @@
+package com.quartz.boot_settingweb.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class settingTest {
+    
+    @RequestMapping("/test") 
+    public ModelAndView test() throws Exception{ 
+        ModelAndView mav = new ModelAndView("test"); //test.jsp로 전송
+        
+        List<String> resultList = new ArrayList<String>(); 
+        resultList.add("!!!HELLO WORLD!!!"); 
+        resultList.add("설정 TEST!!!"); 
+        resultList.add("설정 TEST!!!"); 
+        resultList.add("설정 TEST!!!!!"); 
+        resultList.add("설정 TEST!!!!!!"); 
+        
+        mav.addObject("name", "quartz"); 
+        mav.addObject("list", resultList); 
+        
+        return mav; 
+    }
+ 
+}
---
+++ boot_settingweb/src/test/java/com/quartz/boot_settingweb/BootSettingwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartz.boot_settingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSettingwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
